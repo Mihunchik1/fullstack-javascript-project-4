60425122346b186f29efd7232eda6846e5812634
Mihunchik <arinushkin048@mail.ru>
2025-01-29 21:40:02 +0300

    add test

---
+++ __tests__/download.test.js
@@ -0,0 +1,40 @@
+import { test, expect, beforeAll, afterAll, describe } from '@jest/globals';
+import fs from 'fs/promises';
+import path from 'path';
+import os from 'os';
+import nock from 'nock';
+import pageLoader, { createFileName } from '../src/index.js';
+
+describe('file is downloaded', () => {
+  let tempDir;
+  const url = 'https://example.com';
+  const testData = '<html><body>Example</body></html>';
+
+  beforeAll(async () => {
+    tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+
+    nock('https://example.com')
+      .get('/')
+      .reply(200, testData);
+  });
+
+  test('file has been created and the content matches', async () => {
+
+    await pageLoader(url, tempDir);
+
+    const fileName = createFileName(url);
+    const outputPath = path.join(tempDir, fileName);
+
+    const fileExists = await fs.access(outputPath).then(() => true).catch(() => false);
+    expect(fileExists).toBe(true);
+
+    const fileContent = await fs.readFile(outputPath, 'utf-8');
+    expect(fileContent).toBe(testData);
+  });
+
+  afterAll(async () => {
+    await fs.rmdir(tempDir, { recursive: true });
+
+    nock.cleanAll();
+  });
+});
--- src/index.js
+++ src/index.js
@@ -2,30 +2,30 @@ import axios from 'axios';
 import fs from 'fs/promises';
 import path from 'path';
 
-const createFileName = (url) => {
+export const createFileName = (url) => {
   const urlWithoutProtocol = url.split('//')[1];
   const result = `${urlWithoutProtocol.replace(/[^\w]/g, '-')}.html`;
   return result;
 };
 
 export default (url, option = '/home/user/current-dir') => {
-  const promise = axios.get(url)
-    .then((response) => response.data)
-    .catch((error) => {
-      console.error(error);
-    });
-
   const currentDirectory = process.cwd();
   const fileName = createFileName(url);
+  const outputPath = option === '/home/user/current-dir' ? path.join(currentDirectory, fileName) : path.join(option, fileName);
+  const dirPath = path.dirname(outputPath);
 
-  const outputPath = option === '/home/user/current-dir' ? path.join(currentDirectory, fileName) : path.join(currentDirectory + option, fileName);
-
-  promise
-    .then((data) => fs.writeFile(outputPath, data))
+  return fs.access(dirPath, fs.constants.F_OK)
+    .then(() => axios.get(url))
+    .then((response) => fs.writeFile(outputPath, response.data))
     .then(() => {
       console.log(outputPath);
     })
-    .catch((error) => {
-      console.error(error);
+    .catch((err) => {
+      if (err.code === 'ENOENT') {
+        console.error('dir is not exist');
+      } else {
+        console.error(err);
+      }
+      return Promise.reject(err);
     });
 };
